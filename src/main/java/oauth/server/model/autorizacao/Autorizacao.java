ee2643a8c812e87e9b2336a270521bc93610607e
Kleber Oliveira <klebermo@gmail.com>
2020-02-21 17:24:30 -0300

    model Autorizacao

--- src/main/java/oauth/server/model/autorizacao/Autorizacao.java
+++ src/main/java/oauth/server/model/autorizacao/Autorizacao.java
@@ -1,11 +1,35 @@
 package oauth.server.model.autorizacao;
 
 import javax.persistence.Entity;
+import javax.persistence.Id;
+import javax.persistence.Column;
 import org.springframework.security.core.GrantedAuthority;
 
 @Entity
 public class Autorizacao implements GrantedAuthority {
+  @Id
+  private Integer id;
+
+  @Column
+  private String name;
+
+  public Integer getId() {
+    return id;
+  }
+
+  public void setInteger(Integer id) {
+    this.id = id;
+  }
+
+  public String getName() {
+    return name;
+  }
+
+  public void setName(String name) {
+    this.name = name;
+  }
+
   public String getAuthority() {
-    return null;
+    return name;
   }
 }
