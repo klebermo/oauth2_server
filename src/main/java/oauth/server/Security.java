627c5d375f92c4be0dd2996d455988f807815e29
Kleber Oliveira <klebermo@gmail.com>
2020-02-21 14:47:45 -0300

    Security.java

--- src/main/java/oauth/server/Security.java
+++ src/main/java/oauth/server/Security.java
@@ -3,10 +3,8 @@ package oauth.server;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.core.annotation.Order;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
-import org.springframework.context.annotation.Bean;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.core.userdetails.UserDetailsService;
-import org.springframework.security.provisioning.InMemoryUserDetailsManager;
-import org.springframework.security.core.userdetails.User;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.context.annotation.Bean;
 import org.springframework.security.authentication.AuthenticationManager;
@@ -14,21 +12,13 @@ import org.springframework.security.authentication.AuthenticationManager;
 @EnableWebSecurity
 @Order(1)
 public class Security extends WebSecurityConfigurerAdapter {
-    @Bean
-    @Override
-    public UserDetailsService userDetailsService() {
-        return new InMemoryUserDetailsManager(
-            User.withDefaultPasswordEncoder()
-                .username("enduser")
-                .password("password")
-                .roles("USER")
-                .build());
-    }
+    @Autowired
+    private UserDetailsService userDetailsService;
 
     @Override
     public void configure(AuthenticationManagerBuilder auth) throws Exception {
       auth
-        .userDetailsService(userDetailsService());
+        .userDetailsService(userDetailsService);
     }
 
     @Bean
