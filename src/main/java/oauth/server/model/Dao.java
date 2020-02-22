85ed1378f97c0e121bb694f1f1d5e3d41fd0a799
Kleber Mota de Oliveira <klebermo@gmail.com>
2020-02-22 07:43:25 -0300

    Update Dao.java

--- src/main/java/oauth/server/model/Dao.java
+++ src/main/java/oauth/server/model/Dao.java
@@ -1,31 +1,66 @@
 package oauth.server.model;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import javax.persistence.EntityManagerFactory;
+import javax.persistence.EntityManager;
 import java.util.List;
 
 public class Dao<E> {
+  @Autowired
+  private EntityManagerFactory factory;
+
   private Class<E> classe;
 
   public Dao(Class<E> classe) {
     this.classe = classe;
   }
 
-  public void insert(E object) {
-    //
+  public EntityManager getEntityManager() {
+    return factory.createEntityManager();
   }
 
-  public void update(E object) {
-    //
+  public E insert(E object) {
+    EntityManager entityManager = getEntityManager();
+    entityManager.getTransaction().begin();
+    entityManager.persist(object);
+    entityManager.getTransaction().commit();
+    entityManager.close();
+    return object;
   }
 
-  public void delete(E object) {
-    //
+  public E update(E object) {
+    EntityManager entityManager = getEntityManager();
+    entityManager.getTransaction().begin();
+    entityManager.merge(object);
+    entityManager.getTransaction().commit();
+    entityManager.close();
+    return object;
   }
 
-  public List select() {
-    return null;
+  public E delete(E object) {
+    EntityManager entityManager = getEntityManager();
+    entityManager.getTransaction().begin();
+    entityManager.remove(entityManager.contains(object) ? object : entityManager.merge(object));
+    entityManager.getTransaction().commit();
+    entityManager.close();
+    return object;
+  }
+
+  public List<E> select() {
+    EntityManager entityManager = getEntityManager();
+		entityManager.getTransaction().begin();
+		List<E> lista = entityManager.createQuery("SELECT a FROM "+classe.getSimpleName()+" a").getResultList();
+		entityManager.getTransaction().commit();
+		entityManager.close();
+    return lista;
   }
 
   public List findBy(String key, Object value) {
-    return null;
+    EntityManager entityManager = getEntityManager();
+    entityManager.getTransaction().begin();
+    List result = entityManager.createQuery("SELECT a FROM "+classe.getSimpleName()+" a WHERE a."+key+" = :value").setParameter("value", value).getResultList();
+    entityManager.getTransaction().commit();
+    entityManager.close();
+    return result;
   }
 }
