f79cb9259f5ed11ba1ef0488bedb53b00262051a
Kleber Mota de Oliveira <klebermo@gmail.com>
2020-02-22 07:42:57 -0300

    data.sql

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -5,6 +5,7 @@ insert into usuario (id, username, password) values (1, 'kleber', '');
 insert into usuario_autorizacoes values (1,1);
 insert into usuario_autorizacoes values (1,2);
 
-insert into cliente (client_id, client_secret, authorized_grant_types, access_token_validity_seconds, refresh_token_validity_seconds, redirect_uris, resource_id, scope, auto_approve, scoped, secret_required) values ('first-client', 'noonewilleverguess', 'authorization_code', 3600, 3600, 'http://localhost:8081/', '', 'read', true, true, true);
-insert into cliente_autorizacoes values (1,1);
-insert into cliente_autorizacoes values (1,2);
+insert into cliente (id, client_id, client_secret, access_token_validity_seconds, refresh_token_validity_seconds, auto_approve, scoped, secret_required) values (1, 'first-client', 'noonewilleverguess', 3600, 3600, true, true, true);
+insert into cliente_authorized_grant_types values (1, 'authorization_code');
+insert into cliente_redirect_uris values (1, 'http://localhost:8081');
+insert into cliente_scope values (1, 'read');
