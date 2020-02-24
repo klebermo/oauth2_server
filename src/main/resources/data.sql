a11fa4335f15e7036775a99d2942873fc3909fd2
Kleber Mota de Oliveira <klebermo@gmail.com>
2020-02-24 14:00:59 -0300

    Update data.sql

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -2,7 +2,7 @@ insert into credencial (nome) values ('basic');
 insert into autorizacao (nome) values ('read');
 insert into autorizacao (nome) values ('write');
 
-insert into usuario (username, password, first_name, last_name, email, enabled, locked) values ('kleber', '$2a$04$q8j56WcbSTMViRYqBOfGPOVbUKqSR0tdUoa9awU/cA/TRdPA/krRW', 'Kleber', 'Mota', 'kleber@mail.com', true, false);
+insert into usuario (username, password, first_name, last_name, email, enabled, locked) values ('kleber', '$2y$04$I7gWK8SNW8aLynMW334dPOcfCKjYFLNFZywsr8R0wJzgXjs.mNJ3q', 'Kleber', 'Mota', 'kleber@mail.com', true, false);
 insert into usuario_credenciais values (1,1);
 
 insert into oauth_client_details (client_id,client_secret,authorized_grant_types,access_token_validity,refresh_token_validity,web_server_redirect_uri,resource_ids,scope,scoped,secret_required,autoapprove) values ('first-client', '', 'authorizantion_code', 36000, 36000, 'http://localhost:8081', null, 'read', true, true, true);
