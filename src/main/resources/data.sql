3f15ac78142d79bb01eff080078568aabc411e21
Kleber Mota de Oliveira <klebermo@gmail.com>
2020-02-25 09:34:07 -0300

    Update data.sql

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -1,8 +1,10 @@
 insert into credencial (nome) values ('basic');
 insert into autorizacao (nome) values ('read');
 insert into autorizacao (nome) values ('write');
+insert into credencial_autorizacoes values (1,1);
+insert into credencial_autorizacoes values (1,2);
 
 insert into usuario (username, password, first_name, last_name, email, enabled, locked) values ('kleber', '$2y$04$I7gWK8SNW8aLynMW334dPOcfCKjYFLNFZywsr8R0wJzgXjs.mNJ3q', 'Kleber', 'Mota', 'kleber@mail.com', true, false);
 insert into usuario_credenciais values (1,1);
 
-insert into oauth_client_details (client_id,client_secret,authorized_grant_types,access_token_validity,refresh_token_validity,web_server_redirect_uri,resource_ids,scope,scoped,secret_required,autoapprove) values ('first-client', '', 'authorizantion_code', 36000, 36000, 'http://localhost:8081', null, 'read', true, true, true);
+insert into oauth_client_details (client_id,client_secret,authorized_grant_types,access_token_validity,refresh_token_validity,web_server_redirect_uri,resource_ids,scope,scoped,secret_required,autoapprove) values ('first-client', '$2y$04$rQ.nyRuINjsHn9OCBKr5vO4lLu9Ktxo6NP5HI3UjASyHlmvPSZsdm', 'authorization_code', 36000, 36000, 'http://localhost:8081/', null, 'read', true, true, true);
